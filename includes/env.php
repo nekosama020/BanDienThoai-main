bcc4662869ea3250615ccf84dde593a9b7af6824
nekosama020 <vudung02042004@gmail.com>
2025-12-09 07:51:27 +0700

    Hoan thien he thong CI/CD tu dong

--- includes/env.php
+++ includes/env.php
@@ -1,4 +1,4 @@
 <?php
 // File này chứa bí mật, TUYỆT ĐỐI KHÔNG ĐẨY LÊN GITHUB
-define('GEMINI_API_KEY', 'AIzaSyAP1Jg3EgDsHyvJjN8B2jLQ1OFN7nsjf8E'); // Dán key MỚI của bạn vào đây
+define('GEMINI_API_KEY', 'AIzaSyB_ZsE-RkhITgVIME78emkgx0P4sX_wUsw'); // Dán key MỚI của bạn vào đây
 ?>
